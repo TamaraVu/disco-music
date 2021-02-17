7bd97e9b0c42a8e45a0b1d9f713dd723720b7db2
PC <TamaraVu>
2021-02-17 18:40:39 +0100

    Disco music project

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {LessonsComponent} from './lessons/lessons.component';
+import {LessonTenComponent} from './lessons/lesson-ten/lesson-ten.component';
+
+const routes: Routes = [
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'lessons', component: LessonsComponent},
+  {path: 'lesson', component: LessonTenComponent},
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'}
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule{}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SidebarNavComponent } from './sidebar-nav/sidebar-nav.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import {AppRoutingModule} from './app-routing.module';
+import { LessonsComponent } from './lessons/lessons.component';
+import {DropdownDirective} from './dropdown.directive';
+import { LessonTenComponent } from './lessons/lesson-ten/lesson-ten.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarNavComponent,
+    DashboardComponent,
+    LessonsComponent,
+    DropdownDirective,
+    LessonTenComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
